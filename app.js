eb297c9b9c162b42080527983722fa9f64f14eb3
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-20 17:51:40 -0700

    Tired

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ while (randomPoke1.id === randomPoke2.id) {
 
 const labels = document.querySelectorAll('label');
 
+const firstLabel = labels[0];
+const span1 = firstLabel.children[0];
+const input1 = firstLabel.children[1];
+const img1 = firstLabel.children[2];
+
+const secondLabel = labels[0];
+const span2 = secondLabel.children[0];
+const input2 = secondLabel.children[1];
+const img2 = secondLabel.children[2];
+
+const thirdLabel = labels[0];
+const span3 = thirdLabel.children[0];
+const input3 = thirdLabel.children[1];
+const img3 = thirdLabel.children[2];
+
+
+
 
 
 
