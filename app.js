8b5f83b89b3bb729fecd771d769d35825256ac01
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-20 17:25:26 -0700

    Added getRandomPoke function

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 // import functions and grab DOM elements
-
+import { getRandomPoke } from './pokeutils.js';
+import Pokedex from './Pokedex.js';
 // initialize state
-
+const nextButton = document.getElementById('userChoice');
+const pokemonDiv = document.querySelector('#pokemon');
 // set event listeners to update state and DOM
+
+let pokemonEncounter = 0;
+let capture = [];
+let rounds = 0;
+let notCaptured = Pokedex.slice();
+
+const randomPoke1 = getRandomPoke();
+let randomPoke2 = getRandomPoke();
+let randomPoke3  = getRandomPoke();
+
+while (randomPoke1.id === randomPoke2.id) {
+    randomPoke2 = getRandomPoke(notCaptured);
+}
+
+const labels = document.querySelectorAll('label');
+
+
+
+
---
+++ pokeutils.js
@@ -0,0 +1,4 @@
+export function getRandomPoke(pokedex) {
+    const randomIndex = Math.floor(Math.random() * pokedex.length);
+    return pokedex[randomIndex];
+}
