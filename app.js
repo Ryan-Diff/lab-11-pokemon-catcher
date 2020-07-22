014095fe0b411fb4b3302bad0f7e59618ba744b7
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-22 11:31:02 -0700

    Added functions

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 // import functions and grab DOM elements
-import { getRandomPoke } from './pokeutils.js';
+import { getRandomPoke, getCurrentPokemonArray, addPokemonCaptures, addPokemonEncounters } from './pokeutils.js';
 import Pokedex from './Pokedex.js';
 const totalRounds = document.getElementById('total-rounds');
 const totalPokeSeen = document.getElementById('total-pokemon-seen');
 const pokeImgTags = document.querySelectorAll('img');
 const pokeRadioTags = document.querySelectorAll('input');
-const nextButton = document.getElementById('next-button');
-const totalPokemonCaptured = document.getElementById('total-pokemon-captured');
+// const nextButton = document.getElementById('next-button');
+// const totalPokemonCaptured = document.getElementById('total-pokemon-captured');
 // initialize state
-let pokemonCaptured = 0;
+// let pokemonCaptured = 0;
 const pokemon = Pokedex.slice();
 let gameRounds = 0;
 let totalPokemonSeenCounter = 3;
+let pokemonEncountered = [];
+let localStoragePokemon = getCurrentPokemonArray;
+
 
 // set event listeners to update state and DOM
 
@@ -33,28 +36,39 @@ function newDeck() {
         pokeImgTags[1].src = randomPokemon2.url_image;
         pokeImgTags[2].src = randomPokemon3.url_image;
     }
+
+    pokeRadioTags.forEach((radioTag, i) => {
+        if (i === 0) {
+            radioTag.value = randomPokemon1.pokemon;
+        } else if (i === 1) {
+            radioTag.value = randomPokemon2.pokemon;
+        } else if (i === 2) {
+            radioTag.value = randomPokemon3.pokemon;
+        }
+    });
+
+    addPokemonEncounters(localStoragePokemon, randomPokemon1.pokemon, randomPokemon2.pokemon, randomPokemon3.pokemon);
+ 
 }
 
 newDeck();
 
 pokeRadioTags.forEach((radioTag) => {
     radioTag.addEventListener('click', (e) => {
+        pokemonEncountered.push(e.target.value);
 
-        return e;
+        gameRounds = gameRounds + 1;
+        totalRounds.textContent = `Round: ${gameRounds + 1}`;
+
+        totalPokemonSeenCounter = totalPokemonSeenCounter + 1;
+        totalPokeSeen.textContent = `Total Pokemon Seen: ${totalPokemonSeenCounter}`;
+
+        addPokemonCaptures(localStoragePokemon, e.target.value);
+
+        newDeck();
     });
 });
 
-nextButton.addEventListener('click', () => {
-    // pokemonCaptured = pokemonCaptured + 1;
-    // totalPokemonCaptured.textContent = `Pokemon Captured ${TotalPokemonCaptured}`;
-    gameRounds = gameRounds + 1;
-    totalRounds.textContent = `Round: ${gameRounds + 1}`;
-    totalPokemonSeenCounter = totalPokemonSeenCounter + 3;
-    totalPokeSeen.textContent = `Total Pokemon Seen: ${totalPokemonSeenCounter}`;
-    
-
-    newDeck();
-});
 
 
 
